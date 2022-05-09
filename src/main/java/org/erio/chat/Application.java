3086813952e94ca0304fcf8f25c0338789a48cd6
Sergey Bagrintsev <bagrskoeyl@gmail.com>
2022-05-09 14:23:09 +0300

    intermediate fix

--- src/main/java/org/erio/chat/Application.java
+++ src/main/java/org/erio/chat/Application.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class App {
+public class Application {
 
     public static void main(String[] args) throws IOException, JMSException {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
